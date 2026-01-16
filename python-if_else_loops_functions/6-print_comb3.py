9acfb083a331565c263b63bdfabbc8b7bc13c7aa
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-16 19:56:25 +0300

    fix

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,4 @@ print(
     ", ".join(
         "{}{}".format(num1, num2)
         for num1 in range(10)
-        for num2 in range(num1 + 1, 10)
-    )
-)
-
+        for num2 in range(num1 + 1, 10)))
