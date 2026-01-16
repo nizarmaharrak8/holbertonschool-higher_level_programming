15f9f5a726a459d97d268c0cc631a8e249cf55fb
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-16 20:42:32 +0300

    fix

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,9 +2,5 @@
 def uppercase(str):
     for ch in str:
         print("{}".format(
-            chr(ord(ch) - 32) if 97 <= ord(ch) <= 122 else ch), end='')
+             chr(ord(ch) - 32) if 97 <= ord(ch) <= 122 else ch), end='')
     print()
-
-
-uppercase("best")
-uppercase("Best School 98 Battery street")
