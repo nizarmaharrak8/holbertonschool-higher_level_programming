edd89f0c83a46567fecdd9cdfe649396d77195d0
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-16 18:25:25 +0300

    fix

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for x in range(00, 100):
-    if x == 'e' or x == 'q':
-        continue
-    print("{}".format(x), end=", ")
+for x in range(100):
+    if x < 99:
+        print("{:02}".format(x), end=", ")
+    else:
+        print("{:02}".format(x))
