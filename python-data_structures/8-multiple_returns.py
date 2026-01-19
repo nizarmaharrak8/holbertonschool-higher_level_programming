bf9eacf94762e94f456acfbc018b189a99333233
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-19 18:40:00 +0300

    fix taps

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-	Length = len(sentence)
-	first = sentence[0] if len(sentence) > 0 else None
-	return (Length, first)
-
+    Length = len(sentence)
+    first = sentence[0] if len(sentence) > 0 else None
+    return (Length, first)
