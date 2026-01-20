494f6a5ec4c80040c4511c0ec7dc9c08bf0c8e1d
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-20 18:28:37 +0300

    add

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if matrix is None:
-        matrix = []
-
     new_matrix = []
 
     for row in matrix:
         new_row = []
         for value in row:
-            square = value ** 2
-            new_row.append(square)
+            new_row.append(value ** 2)
         new_matrix.append(new_row)
+
     return new_matrix
