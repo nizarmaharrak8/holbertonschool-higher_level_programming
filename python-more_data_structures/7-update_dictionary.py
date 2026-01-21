9e36ca5d5a92e540b59ceaa2685fc477ea925bc6
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-21 08:15:50 +0000

    fix

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -2,6 +2,7 @@
 def update_dictionary(a_dictionary, key, value):
     if key in a_dictionary:
         a_dictionary[key] = value
+        return a_dictionary
     else:
         a_dictionary[key] = value
         return a_dictionary
