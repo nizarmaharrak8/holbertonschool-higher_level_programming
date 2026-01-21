fbc3e3b66d35a81bb995c4652228e16be3bc344d
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-21 18:32:55 +0300

    fix tab

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python
 def multiply_by_2(a_dictionary):
+    new_dictionary = {}
+
     for key in a_dictionary:
         new_dictionary[key] = a_dictionary[key] * 2
+
     return new_dictionary
