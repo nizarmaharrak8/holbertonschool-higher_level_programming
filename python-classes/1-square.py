b28d6a9b91ad8241589fce3390947c131cada7cd
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-25 18:37:34 +0300

    add

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Docstring for python-classes.0-square
+"""
+
+
+class Square:
+    """
+    Docstring for Square
+    """
+    def __init__(self, size):
+        self.__size = size
