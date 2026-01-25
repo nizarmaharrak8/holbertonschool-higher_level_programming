51cadb7b07a769ff48c636efcd6228c928f18c46
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-25 20:16:55 +0300

    fixed style

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """Docstring for python-classes.0-square"""
 
-
 class Square:
     """
     Docstring for Square
     """
-
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
