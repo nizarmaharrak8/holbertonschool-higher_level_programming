8c05848a35c7679b5d9679287f9085a0f04251cf
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-26 07:06:01 +0000

    fix

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -2,7 +2,7 @@
 
 class Square:
     def __init__(self, size=0):
-        if not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
