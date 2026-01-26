c4b6150d8ddd50be0374d672681972a37d103c25
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-26 19:23:39 +0300

    fix tabs

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -29,19 +29,19 @@ class Square:
     @property
     def position(self):
         """
-		Docstring for position
-		
-		:param self: Description
-		"""
+        Docstring for position
+        
+        :param self: Description
+        """
         return self.__size
     @position.setter
     def position(self, value):
         """
-		Docstring for position
-		
-		:param self: Description
-		:param value: Description
-		"""
+        Docstring for position
+        
+        :param self: Description
+        :param value: Description
+        """
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if not all(isinstance(x, int) and x >= 0 for x in value):
