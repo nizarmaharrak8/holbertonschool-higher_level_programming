21130a5cda0b89b0019211ee64d838243f8e609a
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-26 06:50:37 +0000

    fix

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,18 +1,14 @@
-#!/usr/bin/python3
-"""Docstring for python-classes.0-square"""
-
 class Square:
-    """
-        Docstring for Square
-    """
+    """Docstring for Square"""
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    
+
     def area(self):
-        """"Calculates and returns the area of the square."""
+        """Calculates and returns the area of the square."""
         return self.__size ** 2
 
