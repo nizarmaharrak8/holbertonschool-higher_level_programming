9fe9f7f596c66d3be00b5580def8606192d65001
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-26 07:07:03 +0000

    fix

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
+"""Defines a Square class."""
 
 class Square:
+    """Docstring for Square"""
+
     def __init__(self, size=0):
-        if type(size) is not int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
+        """Calculates and returns the area of the square."""
         return self.__size ** 2
 
