e968523b14514af9d6d47dcaa320284c74558f29
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-26 09:53:06 +0000

    fix

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""Defines a Square class."""
+"""Defines a Square class with size validation."""
 
 class Square:
-    """Docstring for Square"""
+    """
+    Represents a square with a private size attribute.
+    """
 
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self._size = size
 
     def area(self):
-
-        """Calculates and returns the area of the square."""
-
-        return self.__size ** 2
+        """Calculates the area of the square."""
+        return self._size ** 2
 
