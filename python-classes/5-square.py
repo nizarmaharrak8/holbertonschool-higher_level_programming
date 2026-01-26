86be3e32e0de2f1ed4d2291e80f55e4ea685475d
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-26 11:41:29 +0000

    fix

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -17,8 +17,10 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for _ in range(self.__size):
-                print("#" * self.__size)
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
 
     @property
     def size(self):
