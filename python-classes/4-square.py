1468636d4528ae44d3aee9171cfd0a954bd96b89
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-26 10:57:10 +0000

    fix

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-"""Docstring for python-classes.0-square"""
+"""Defines a Square class with size validation."""
+
 
 class Square:
     """
-        Docstring for Square
+       Represents a square with a private size attribute.
     """
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    
+
     def area(self):
-        """"Calculates and returns the area of the square."""
+        """Calculates the area of the square."""
         return self.__size ** 2
     @property
     def size(self):
