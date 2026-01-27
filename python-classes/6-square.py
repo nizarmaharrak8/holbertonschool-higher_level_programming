7dab562209e298b5cad7bd66b85c07b2999b892d
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-27 06:39:55 +0000

    fix

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -18,7 +18,7 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
@@ -30,15 +30,15 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2:
+        if (
+            type(value) is not tuple or
+            len(value) != 2 or
+            type(value[0]) is not int or
+            type(value[1]) is not int or
+            value[0] < 0 or
+            value[1] < 0
+        ):
             raise TypeError("position must be a tuple of 2 positive integers")
-
-        if not isinstance(value[0], int) or value[0] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-
-        if not isinstance(value[1], int) or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-
         self.__position = value
 
     def my_print(self):
