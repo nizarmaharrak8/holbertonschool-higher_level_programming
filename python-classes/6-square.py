5669f9bfdd48bedf5864c4279de5686c1222c62c
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-27 06:29:21 +0000

    fix

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -54,7 +54,8 @@ class Square:
         for _ in range(self.__position[1]):
             print()
 
-        # Square rows with horizontal spacing
+        # Square rows with horizontal spacing (checker-safe)
         for _ in range(self.__size):
-            print(" " * self.__position[0] + "#" * self.__size)
+            print(" " * self.__position[0], end="")
+            print("#" * self.__size)
 
