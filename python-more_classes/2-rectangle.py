d2127b038b0044a4811d55cafc7af3f9ae30bccf
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-27 11:04:00 +0000

    fix area fun

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -37,6 +37,10 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
+    def area(self):
+        """Returns the area of the rectangle"""
+        return self.__width * self.__height
+
     def perimeter(self):
         """Returns the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
