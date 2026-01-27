0cc8f7876c135a1155f89ffe5d329bd09c98fb0b
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-27 06:43:19 +0000

    fix

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,24 +1,38 @@
 #!/usr/bin/python3
-"""Defines a Square class."""
+"""Defines a Square class with size validation."""
 
 
 class Square:
-    """Represents a square."""
+    """
+       Represents a square with a private size attribute.
+    """
 
     def __init__(self, size=0, position=(0, 0)):
-        self.size = size
-        self.position = position
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        if not isinstance(position, tuple) or len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        if not all(isinstance(x, int) and x >= 0 for x in position):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__size = size
+        self.__position = position
 
     def area(self):
+        """Calculates the area of the square."""
         return self.__size ** 2
 
     @property
     def size(self):
+        """Getter for the size attribute."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        if type(value) is not int:
+        """Setter for the size attribute with validation."""
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
@@ -26,29 +40,28 @@ class Square:
 
     @property
     def position(self):
+        """Getter for the position attribute."""
         return self.__position
 
     @position.setter
     def position(self, value):
-        if (
-            type(value) is not tuple or
-            len(value) != 2 or
-            type(value[0]) is not int or
-            type(value[1]) is not int or
-            value[0] < 0 or
-            value[1] < 0
-        ):
+        """Setter for the position attribute with validation."""
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        if not all(isinstance(x, int) and x >= 0 for x in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def my_print(self):
+        """
+        Prints the square using the '#' character.
+        """
         if self.__size == 0:
             print()
-            return
-
-        for _ in range(self.__position[1]):
-            print()
-
-        for _ in range(self.__size):
-            print(" " * self.__position[0] + "#" * self.__size)
-
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                lien = ' ' * self.__position[0] + "#" * self.__size
+                print(lien)
