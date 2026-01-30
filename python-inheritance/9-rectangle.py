77924c25cffa8b8dc5d7a4126e82479c245f882e
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-30 22:04:36 +0300

    refactor: remove docstrings from __init__ and area methods in Rectangle class

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -8,7 +8,6 @@ class Rectangle(BaseGeometry):
     """Rectangle class that inherits from BaseGeometry."""
 
     def __init__(self, width, height):
-        """Initialize a Rectangle with width and height."""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
 
@@ -16,9 +15,7 @@ class Rectangle(BaseGeometry):
         self.__height = height
 
     def area(self):
-        """Return the area of the Rectangle."""
         return self.__width * self.__height
 
     def __str__(self):
-        """Return the string representation of the Rectangle."""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
