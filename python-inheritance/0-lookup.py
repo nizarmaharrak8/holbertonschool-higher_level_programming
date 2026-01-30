180db0f223da69ef495d78777bea6e6678101713
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-30 20:50:12 +0300

    feat: add MyList class with sorting functionality and test cases

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -7,3 +7,4 @@ Returns the list of available attributes and methods of an object
 def lookup(obj):
     """Return list of attributes and methods of obj"""
     return dir(obj)
+  
---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Docstring for python-inheritance.1-my_list
+"""
+
+
+class MyList(list):
+    """
+        Docstring for MyList
+        """
+
+    def print_sorted(self):
+        print(sorted(self))
+        
