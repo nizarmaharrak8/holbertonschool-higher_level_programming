d1649d50f587d28fa90ed9e250bbb2cad3e4c5f7
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-30 21:30:05 +0300

    fix: improve docstring formatting and update comparison operator in is_same_class.py

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-"""Module that checks if an object is exactly an instance of a given class."""
+"""
+Module that checks if an object is exactly an instance
+of a given class.
+"""
+
 
 def is_same_class(obj, a_class):
-    """Return True if obj is exactly an instance of a_class, otherwise False."""
-    return type(obj) == a_class
+    """Return True if obj is exactly an instance of a_class."""
+    return type(obj) is a_class
