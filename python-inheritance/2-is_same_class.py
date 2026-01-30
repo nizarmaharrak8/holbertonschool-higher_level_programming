bbf1a9103b0e36a7528af3524b7501ceb37b03f5
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-01-30 21:29:05 +0300

    fix: update docstrings in is_same_class.py for clarity

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+"""Module that checks if an object is exactly an instance of a given class."""
+
 def is_same_class(obj, a_class):
+    """Return True if obj is exactly an instance of a_class, otherwise False."""
     return type(obj) == a_class
