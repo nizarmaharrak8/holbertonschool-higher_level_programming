e88c4fea375039d06021677a3a541b5a7f80a198
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-02-03 17:13:12 +0000

    fix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+Reads a text file (UTF-8) and prints it to stdout.
+"""
+
+
 def read_file(filename=""):
+    """Reads a text file and prints it to stdout"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
 
