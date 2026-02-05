4957261ec81dca0c27ef5fbe361bdf2f2dcf239a
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-02-05 16:01:55 +0000

    add

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -7,4 +7,3 @@ Returns the dictionary description of a class instance for JSON serialization.
 def class_to_json(obj):
     """Returns a dictionary with all serializable attributes of obj"""
     return obj.__dict__
-
