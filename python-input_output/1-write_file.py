5e63f70eb7dcf2ca2728cb9776b7b430eec6c68e
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-02-05 15:27:36 +0000

    fix

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -9,4 +9,3 @@ def write_file(filename="", text=""):
     """Writes text to a file and returns the number of characters"""
     with open(filename, mode="w", encoding="utf-8") as f:
         return f.write(text)
-
