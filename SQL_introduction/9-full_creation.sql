331cf482b0703f56a11500ca92cf889f1383ea60
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-02-26 22:48:42 +0300

    add: insert initial data into second_table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,3 +4,4 @@ CREATE TABLE IF NOT EXISTS second_table (
 	name VARCHAR(256) NOT NULL,
 	score INT NOT NULL,
 );
+INSERT INTO second_table VALUES (1, 'Alice', 85), (2, 'Bob', 90), (3, 'Charlie', 78);
