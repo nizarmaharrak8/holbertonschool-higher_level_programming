0849fba1c558c57297896794eacdca0c7fe82c75
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-02-26 23:15:30 +0300

    fix: format SQL script for second_table by correcting indentation and adding missing newline

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,12 +1,11 @@
--- Write a script that creates a new table called second_table with the following columns:
 CREATE TABLE IF NOT EXISTS second_table (
-	id INT,
-	name VARCHAR(256) NOT NULL,
-	score INT NOT NULL,
+    id INT,
+    name VARCHAR(256) NOT NULL,
+    score INT NOT NULL
 );
+
 INSERT INTO second_table (id, name, score) VALUES
 (1, 'John', 10), 
 (2, 'Alex', 3),
-(3, 'Bob', 14)
-(4, 'George', 8)
-;
+(3, 'Bob', 14),
+(4, 'George', 8);
