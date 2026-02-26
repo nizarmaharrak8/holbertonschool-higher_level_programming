f9c0a9f33b3dc3d8ee1277eb435eee48513abec6
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-02-26 22:55:29 +0300

    add: update initial data in second_table with new records

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,4 +4,9 @@ CREATE TABLE IF NOT EXISTS second_table (
 	name VARCHAR(256) NOT NULL,
 	score INT NOT NULL,
 );
-INSERT INTO second_table VALUES (1, 'Alice', 85), (2, 'Bob', 90), (3, 'Charlie', 78);
+INSERT INTO second_table VALUES 
+(1, 'John', 10), 
+(2, 'Alex', 3),
+(3, 'Bob', 14)
+(4, 'George', 8)
+;
