17dbad18e7c05421699c9911b7bac2f6cb773255
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-02-27 01:09:58 +0300

    refactor: update SQL query to use consistent alias for average score

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,2 +1,3 @@
 -- Write a query to display the average score of all records in second_table.
-SELECT AVG(score) AS average_score FROM second_table;
+SELECT AVG(score) AS average
+FROM second_table;
