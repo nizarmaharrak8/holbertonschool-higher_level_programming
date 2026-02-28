966cdbc6b57c21ae82d31f5dfd3426ac751d4229
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-02-28 19:36:18 +0300

    add

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
--- List all shows with their genre IDs, including shows without any genre
+-- Write a query to find the genre_id for each show. The result should include the show title and genre_id, and be ordered by show title and genre_id in ascending order.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
