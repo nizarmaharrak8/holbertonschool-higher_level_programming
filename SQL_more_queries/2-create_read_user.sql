192d714a85cbade5abcfdb512b1bf8e3712c177a
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-02-28 17:40:08 +0300

    add SQL script to create a user with SELECT privileges on hbtn_0d_2 database

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- 2. Read user
+-- Create a user with only SELECT privileges on a specific database and show its grants.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+FLUSH PRIVILEGES;
