9cdb901ba2049e459a74ed8273df83e8d4563301
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-02-28 17:58:44 +0300

    add

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
--- List all shows with their genre IDs
+-- List all shows with their genre IDs, including shows without any genre
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
