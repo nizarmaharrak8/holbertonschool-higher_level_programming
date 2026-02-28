7e1e84c36ff6d886e9db170b5b9afea150311494
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-02-28 17:47:43 +0300

    add SQL script to create states table with unique name constraint

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,8 +1,11 @@
--- 6. States 
--- Create a database called hbtn_0d_usa and a table called states with the following
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the database
 USE hbtn_0d_usa;
+
+-- Create the table states if it doesn't exist
 CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
+    name VARCHAR(256) NOT NULL UNIQUE
 );
